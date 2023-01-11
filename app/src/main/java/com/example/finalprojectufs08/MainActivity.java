793815b4355fda7e91114dd97b45cbe2fd408818
andre <d.castronunez@itsrizzoli.it>
2023-01-11 16:52:02 +0100

    added bottom menu

--- app/src/main/java/com/example/finalprojectufs08/MainActivity.java
+++ app/src/main/java/com/example/finalprojectufs08/MainActivity.java
@@ -29,21 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db.collection("user")
-                .whereEqualTo("gender", "male")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
 
     }
 }
