3b5cbb10488aec7ded42f5a8332aa3a3cb4c8c63
andre <d.castronunez@itsrizzoli.it>
2023-01-11 17:03:22 +0100

    feature to switch fragments with bottom menu

--- app/src/main/java/com/example/finalprojectufs08/MainActivity.java
+++ app/src/main/java/com/example/finalprojectufs08/MainActivity.java
@@ -4,6 +4,7 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -24,11 +26,36 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    private BottomNavigationView navigationBottomMenu;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navigationBottomMenu =findViewById(R.id.navview);
 
+        navigationBottomMenu.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.homeIconItem:
+                    configFragmentManager(ListaDataFragment.class);
+                    break;
+
+                case R.id.profileIconItem:
+                    configFragmentManager(ProfileFragment.class);
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void configFragmentManager(Class fragmentClass) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragmentClass, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("name")
+                .commit();
     }
 }
